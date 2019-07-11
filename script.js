eecfd319cbe243b8bfade33a1a199bc9231e69cb
Hannah Zuber <hgomberg@gmail.com>
2019-07-11 11:24:21 -0400

    s

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function choosePath(page){
 
 function searchHabitat(){
    $('.second-page-choice').on('click', function(event){
-    var habitat = $(event.target).attr('id');
+    var habitat = $(event.target).closest('div.second-page-choice').attr('id');
     for (let i=0; i< animals.length; i++){
       if (animals[i].habitat.includes(habitat)){
         generateAnimalCard(i);
@@ -173,7 +173,7 @@ function chooseAnimalName(){
 
 function searchType(){
    $('.second-page-choice').on('click', function(event){
-    var type = $(event.target).attr('id');
+    var type = $(event.target).closest('div.second-page-choice').attr('id');
     for (let i=0; i< animals.length; i++){
       if (animals[i].type == type){
         generateAnimalCard(i);
@@ -267,11 +267,11 @@ function verifyAnimal(textInput){
 function getVideo(choice){
   const params= {
     key: apiKey,
-    q: choice + " animal kids learn nature",
+    q: choice + " animal kids nature learn",
     part: "snippet",
     safeSearch: "strict",
     type: "video",
-    maxResults: 25,
+    maxResults: 30,
   };
   const queryString = formatQueryParams(params);
   const url = searchUrl + '?' + queryString;
